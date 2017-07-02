59b28cc7bb4ddb603024912bda31fc06d6e6f69b
Nick Gubbels <nickgubbels@gmail.com>
2017-07-02 17:16:22 +0200

    Move MPlayer actions to an MPlayerWrapper.`

---
+++ NIcolasCodE.DvdAudioExtractor/External/MPlayerWrapper.cs
@@ -0,0 +1,38 @@
+﻿using NIcolasCodE.DvdAudioExtractor.Services;
+using NIcolasCodE.DvdAudioExtractor.Utilities;
+using System;
+using System.IO;
+
+namespace NIcolasCodE.DvdAudioExtractor.External
+{
+    internal class MPlayerWrapper
+    {
+        private const string mPlayerPath = "C:\\Users\\Nick\\Desktop\\MPlayer-x86_64-r37905+g1f5630a\\MPlayer.exe";
+
+        private IOpticalDriveService opticalDriveService;
+
+        private string identifyCache;
+
+        public MPlayerWrapper(IOpticalDriveService opticalDriveService)
+        {
+            this.opticalDriveService = opticalDriveService;
+        }
+
+        public string IdentifyDvd()
+        {
+            if(String.IsNullOrWhiteSpace(identifyCache))
+            {
+                identifyCache = ProcessWithOutput.ExecuteProcess(mPlayerPath, $"-identify dvd:// -dvd-device {opticalDriveService.RetrieveOpticalDriveLetter()}: -frames 0");
+            }
+
+            return identifyCache;
+        }
+
+        public void ExtractAudioToWav(int titleNo, int chapterNo, FileInfo targetFile)
+        {
+            var arguments = $"-vo null -vc null -ao pcm:fast:file={targetFile.Name} dvd://{titleNo} -chapter {chapterNo}-{chapterNo} -dvd-device {opticalDriveService.RetrieveOpticalDriveLetter()}:";
+
+            ProcessWithOutput.ExecuteProcess(mPlayerPath, arguments, targetFile.Directory.FullName, out string stdError);
+        }
+    }
+}
--- NIcolasCodE.DvdAudioExtractor/Services/DvdService.cs
+++ NIcolasCodE.DvdAudioExtractor/Services/DvdService.cs
@@ -1,8 +1,7 @@
-﻿using NIcolasCodE.DvdAudioExtractor.Model;
-using NIcolasCodE.DvdAudioExtractor.Utilities;
+﻿using NIcolasCodE.DvdAudioExtractor.External;
+using NIcolasCodE.DvdAudioExtractor.Model;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
 using System.Text.RegularExpressions;
 
@@ -10,25 +9,23 @@ namespace NIcolasCodE.DvdAudioExtractor.Services
 {
     internal class DvdService : IDvdService
     {
-        private MPlayerDvdIdentifier mPlayerDvdIdentifier;
-        private MPlayerDvdAudioExtractor mPlayerDvdAudioExtractor;
+        private MPlayerWrapper mPlayerWrapper;
 
         public DvdService(IOpticalDriveService opticalDriveService)
         {
-            mPlayerDvdIdentifier = new MPlayerDvdIdentifier(opticalDriveService);
-            mPlayerDvdAudioExtractor = new MPlayerDvdAudioExtractor(opticalDriveService);
+            mPlayerWrapper = new MPlayerWrapper(opticalDriveService);
         }
 
         public List<DvdTitle> RetrieveTitlesAndChapters()
         {
-            var mPlayerResult = mPlayerDvdIdentifier.IdentifyDvd();
+            var mPlayerResult = mPlayerWrapper.IdentifyDvd();
 
             return ParseMPlayerResult(mPlayerResult);
         }
 
         public void ExtractToWav(int titleNo, int chapterNo, string filePath)
         {
-            mPlayerDvdAudioExtractor.ExtractAudioToWav(titleNo, chapterNo, filePath);
+            mPlayerWrapper.ExtractAudioToWav(titleNo, chapterNo, new FileInfo(filePath));
         }
 
         private static List<DvdTitle> ParseMPlayerResult(string mplayerResult)
@@ -55,54 +52,6 @@ namespace NIcolasCodE.DvdAudioExtractor.Services
             }
 
             return result;
-        }
-
-        private class MPlayerDvdIdentifier
-        {
-            private string cache;
-            private IOpticalDriveService opticalDriveService;
-
-            public MPlayerDvdIdentifier(IOpticalDriveService opticalDriveService)
-            {
-                this.opticalDriveService = opticalDriveService;
-            }
-
-            public string IdentifyDvd()
-            {
-                if(String.IsNullOrWhiteSpace(cache))
-                {
-                    cache = DoMPlayerCall();
-                }
-
-                return cache;
-            }
-
-            private string DoMPlayerCall()
-            {
-                var path = "C:\\Users\\Nick\\Desktop\\MPlayer-x86_64-r37905+g1f5630a\\MPlayer.exe";
-                return ProcessWithOutput.ExecuteProcess(path, $"-identify dvd:// -dvd-device {opticalDriveService.RetrieveOpticalDriveLetter()}: -frames 0");
-            }
-        }
-
-        private class MPlayerDvdAudioExtractor
-        {
-            //vlc.exe -I dummy --no-sout-video --sout-audio --no-sout-rtp-sap --no-sout-standard-sap --ttl=1 --sout-keep --sout "#transcode{acodec=s16l}:std{access=file,mux=wav,dst=C:\Users\Nick\Desktop\test1.wav}" dvd:///F:\#1:8-1:8 vlc://quit
-            private IOpticalDriveService opticalDriveService;
-
-            public MPlayerDvdAudioExtractor(IOpticalDriveService opticalDriveService)
-            {
-                this.opticalDriveService = opticalDriveService;
-            }
-
-            public void ExtractAudioToWav(int titleNo, int chapterNo, string fileLocation)
-            {
-                var fileInfo = new FileInfo(fileLocation);
-
-                var path = "C:\\Users\\Nick\\Desktop\\MPlayer-x86_64-r37905+g1f5630a\\MPlayer.exe";
-                var arguments = $"-vo null -vc null -ao pcm:fast:file={fileInfo.Name} dvd://{titleNo} -chapter {chapterNo}-{chapterNo} -dvd-device {opticalDriveService.RetrieveOpticalDriveLetter()}:";
-
-                var output = ProcessWithOutput.ExecuteProcess(path, arguments, fileInfo.Directory.FullName, out string stdError);
-            }
         }
     }
 }
