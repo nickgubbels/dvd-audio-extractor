4024e47a2ad1796efef83757943ee0bb702e20df
Nick Gubbels <nickgubbels@gmail.com>
2017-07-02 17:28:48 +0200

    Move WMI calls to a WmiWrapper.

---
+++ NIcolasCodE.DvdAudioExtractor/External/WmiWrapper.cs
@@ -0,0 +1,47 @@
+﻿using NIcolasCodE.DvdAudioExtractor.Utilities;
+using System.Text.RegularExpressions;
+
+namespace NIcolasCodE.DvdAudioExtractor.External
+{
+    internal class WmiWrapper
+    {
+        private char? driveLetterCache;
+
+        public char? RetrieveOpticalDriveLetter()
+        {
+            if(!driveLetterCache.HasValue)
+            {
+                var wmicResult = ProcessWithOutput.ExecuteProcess("wmic.exe", "logicaldisk get deviceid, drivetype");
+
+                var regex = new Regex("([A-Z]):\\s*(\\d)");
+
+                foreach (Match match in regex.Matches(wmicResult))
+                {
+                    if (match.Groups.Count == 3)
+                    {
+                        if (int.TryParse(match.Groups[2].Value, out int driveType)
+                            && driveType == (int)WmiDeviceTypes.CompactDisc
+                            && match.Groups[1].Value.Length == 1)
+                        {
+                            driveLetterCache = match.Groups[1].ToString()[0];
+                            break;
+                        }
+                    }
+                }
+            }
+
+            return driveLetterCache;
+        }
+
+        private enum WmiDeviceTypes
+        {
+            Unknown = 0,
+            NoRootDirectory = 1,
+            RemovableDisk = 2,
+            LocalDisk = 3,
+            NetworkDrive = 4,
+            CompactDisc = 5,
+            RAMDisk = 6
+        }
+    }
+}
--- NIcolasCodE.DvdAudioExtractor/Services/OpticalDriveService.cs
+++ NIcolasCodE.DvdAudioExtractor/Services/OpticalDriveService.cs
@@ -1,52 +1,20 @@
-﻿using NIcolasCodE.DvdAudioExtractor.Utilities;
-using System.Text.RegularExpressions;
+﻿using NIcolasCodE.DvdAudioExtractor.External;
 
 namespace NIcolasCodE.DvdAudioExtractor.Services
 {
     internal class OpticalDriveService : IOpticalDriveService
     {
+        private WmiWrapper wmiWrapper;
+
+        public OpticalDriveService()
+        {
+            wmiWrapper = new WmiWrapper();
+        }
+
         public char? RetrieveOpticalDriveLetter()
         {
             //TODO: Use the following priority: 1) command-line argument, 2) app.config setting, 3) WMI command-line result
-            return RetrieveOpticalDriveLetterUsingWindowsManagementInstrumentationCommandLine();
-        }
-
-        private enum WmiDeviceTypes
-        {
-            Unknown = 0,
-            NoRootDirectory = 1,
-            RemovableDisk = 2,
-            LocalDisk = 3,
-            NetworkDrive = 4,
-            CompactDisc = 5,
-            RAMDisk = 6
-        }
-
-        private static char? RetrieveOpticalDriveLetterUsingWindowsManagementInstrumentationCommandLine()
-        {
-            var wmicResult = DoWmicCall();
-
-            var regex = new Regex("([A-Z]):\\s*(\\d)");
-
-            foreach (Match match in regex.Matches(wmicResult))
-            {
-                if (match.Groups.Count == 3)
-                {
-                    if (int.TryParse(match.Groups[2].Value, out int driveType)
-                        && driveType == (int)WmiDeviceTypes.CompactDisc
-                        && match.Groups[1].Value.Length == 1)
-                    {
-                        return match.Groups[1].ToString()[0];
-                    }
-                }
-            }
-
-            return null;
-        }
-
-        private static string DoWmicCall()
-        {
-            return ProcessWithOutput.ExecuteProcess("wmic.exe", "logicaldisk get deviceid, drivetype");
+            return wmiWrapper.RetrieveOpticalDriveLetter();
         }
     }
 }
